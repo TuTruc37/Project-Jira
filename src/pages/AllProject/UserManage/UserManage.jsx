66ce02185784d3880a3e43c96bebd326162747da
AnthonyNguyen98 <nmTan2009@gmail.com>
2024-07-11 17:27:18 +0700

    wip

--- src/pages/AllProject/UserManage/UserManage.jsx
+++ src/pages/AllProject/UserManage/UserManage.jsx
@@ -153,7 +153,7 @@ const UserManage = () => {
   }
 
   const checkPhoneValid = (e) => {
-    const phoneRegex = /^(03|08|07)\d{7}$/;
+    const phoneRegex = /^(03|08|07|09)\d{8}$/;
     const value = e.target.value;
     setPhone(value);
     setIsValid(phoneRegex.test(value));
